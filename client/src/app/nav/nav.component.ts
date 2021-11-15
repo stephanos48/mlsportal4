2ad1c6614222e4cee57f4e042429c91225138bfb
Stephen English <stephanos.english@gmail.com>
2021-11-15 14:32:29 -0600

    addlanding

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -20,7 +20,7 @@ export class NavComponent implements OnInit {
 
   login(){
     this.accountService.login(this.model).subscribe(response => {
-      this.router.navigateByUrl('/members');
+      this.router.navigateByUrl('/landing');
     })
   }
 
